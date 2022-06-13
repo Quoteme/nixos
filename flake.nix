81df2e2576a2e86d2305fb424f4a527aab3b6a8e
Luca Leon Happel <lucahappel99@gmx.de>
2022-06-13 23:01:42 +0200

    adding cordova for android development

--- flake.nix
+++ flake.nix
@@ -345,6 +345,7 @@
                 libmtp
                 usbutils
                 scrcpy
+                nodePackages.cordova
               # Flutter
                 android-tools
                 android-studio
