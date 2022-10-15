efe565f4f47b96467a4fdee9f65a2f8a20bee06c
Luca Leon Happel <lucahappel99@gmx.de>
2022-10-15 14:21:52 +0200

    remove i3lock from xidlehook

--- scripts/xidlehook.sh
+++ scripts/xidlehook.sh
@@ -13,10 +13,6 @@ xidlehook \
   --timer 60 \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 10 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; i3lock' \
-    '' \
   `# Finally, suspend an hour after it locks` \
   --timer 3600 \
     'systemctl suspend' \
