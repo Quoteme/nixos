5fb3f7c9963a1f4fa39ac7680c48042317a227f6
Luca Leon Happel <lucahappel99@gmx.de>
2023-01-19 22:55:10 +0100

    reenable picom

--- home.nix
+++ home.nix
@@ -74,7 +74,7 @@
   };
   services.picom = {
     # disabled for now. Configure multiple monitors someday
-    enable = false;
+    enable = true;
     shadow = true;
     shadowOpacity = "0.8";
     fade = true;
