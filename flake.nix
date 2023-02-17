5a8c4230fc4e3e3c9e8d7dcdd82cf38e3e88710c
Luca Leon Happel <lucahappel99@gmx.de>
2023-02-17 16:28:19 +0100

    add steam controller support

--- flake.nix
+++ flake.nix
@@ -556,14 +556,14 @@
 
             # development
             java.enable = true;
-
-            # Gaming
-            steam = {
-              enable = true;
-              remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-              dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-            };
           };
+          # Gaming
+          programs.steam = {
+            enable = true;
+            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+          };
+          hardware.steam-hardware.enable = true;
 
           # Shell configuration
           environment.variables = {
