b4c9ba24591ae879c7b5a86aa713117a907dbcfd
Luca Leon Happel <luca.happel@hhu.de>
2023-08-26 12:53:21 +0200

    add: docker modulatization

---
+++ modules/applications/virtualisation/docker.nix
@@ -0,0 +1,30 @@
+{ config
+, options
+, lib
+, pkgs
+, ...
+}@inputs:
+let
+  inherit (builtins) pathExists readFile;
+  inherit (lib.modules) mkIf;
+  system = "x86_64-linux";
+  cfg = config.modules.applications.virtualisation.docker;
+in
+{
+  options.modules.applications.virtualisation.docker =
+    let
+      inherit (lib.options) mkEnableOption mkOption;
+      inherit (lib.types) nullOr path;
+    in
+    {
+      enable = mkEnableOption "Enable Docker";
+    };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker.enable = true;
+    virtualisation.docker.enableOnBoot = false;
+    virtualisation.docker.enableNvidia = true;
+    virtualisation.docker.storageDriver = "btrfs";
+    users.users.luca.extraGroups = [ "docker" ];
+  };
+}
