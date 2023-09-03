e14a18240674f620dbae645d677111add2ef539f
Luca Leon Happel <lucahappel99@gmx.de>
2023-09-03 16:15:09 +0200

    fix: multiple kde problems

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -23,7 +23,7 @@ in
   config = mkIf cfg.enable {
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = true;
-    services.xserver.displayManager.sddm.theme = "chili";
+    services.xserver.displayManager.sddm.theme = "breeze";
     services.xserver.desktopManager.plasma5.enable = true;
     programs.dconf.enable = true;
     programs.kdeconnect.enable = true;
@@ -36,6 +36,15 @@ in
       libsForQt5.kio-gdrive
       libsForQt5.plasma-integration
       libsForQt5.plasma-nm
+      # libsForQt5.akonadi
+      # libsForQt5.akonadi-mime
+      # libsForQt5.akonadi-notes
+      # libsForQt5.akonadiconsole
+      # libsForQt5.akonadi-search
+      # libsForQt5.akonadi-contacts
+      # libsForQt5.akonadi-calendar
+      # libsForQt5.akonadi-calendar-tools
+      # libsForQt5.akonadi-import-wizard
       libsForQt5.kalendar
       libsForQt5.accounts-qt
       libsForQt5.mauikit-accounts
@@ -61,7 +70,11 @@ in
       aspellDicts.en-science
       config.nur.repos.baduhai.koi
       # SDDM
-      unstable.sddm-chili-theme
+      # unstable.sddm-chili-theme
     ];
+    # 
+    environment.sessionVariables = {
+      QT_QUICK_CONTROLS_STYLE = "org.kde.desktop";
+    };
   };
 }
