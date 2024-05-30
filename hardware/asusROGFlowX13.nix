0a4eee0bd65e978e671ce43f211d7f55bd164d1f
Luca Leon Happel <lucahappel99@gmx.de>
2024-05-30 22:25:29 +0200

    fix: use correct settings to disable nvidia

--- hardware/asusROGFlowX13.nix
+++ hardware/asusROGFlowX13.nix
@@ -232,11 +232,7 @@
   specialisation = {
     on-the-go.configuration = {
       system.nixos.tags = [ "on-the-go" ];
-      hardware.nvidia = {
-        prime.offload.enable = lib.mkForce true;
-        prime.offload.enableOffloadCmd = lib.mkForce true;
-        prime.sync.enable = lib.mkForce false;
-      };
+      hardware.nvidiaOptimus.disable = lib.mkForce true;
     };
   };
 }
