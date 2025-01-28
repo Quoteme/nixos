8bf691fb82204294af13e5278d40d77a39bef108
Luca Leon Happel <lucahappel99@gmx.de>
2025-01-28 15:53:03 +0100

    add: ollama

---
+++ modules/applications/ai/ollama.nix
@@ -0,0 +1,19 @@
+{ config, options, lib, pkgs, ... }@inputs:
+let
+  inherit (builtins) pathExists readFile;
+  inherit (lib.modules) mkIf;
+  system = "x86_64-linux";
+  cfg = config.modules.applications.ai.ollama;
+in {
+  options.modules.applications.ai.ollama = let
+    inherit (lib.options) mkEnableOption mkOption;
+    inherit (lib.types) nullOr path;
+  in { enable = mkEnableOption "Enable Ollama"; };
+
+  config = mkIf cfg.enable {
+    services.ollama = {
+      enable = true;
+      acceleration = "cuda";
+    };
+  };
+}
