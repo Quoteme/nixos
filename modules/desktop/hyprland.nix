37329810683006698144b0047d43a65b48b7cd2b
Luca Leon Happel <lucahappel99@gmx.de>
2025-06-02 16:26:43 +0200

    add: hyprland

---
+++ modules/desktop/hyprland.nix
@@ -0,0 +1,21 @@
+{ config, options, lib, pkgs, ... }@inputs:
+let
+  inherit (builtins) pathExists readFile;
+  inherit (lib.modules) mkIf;
+
+  cfg = config.modules.desktop.hyprland;
+in {
+  options.modules.desktop.hyprland = let
+    inherit (lib.options) mkEnableOption mkOption;
+    inherit (lib.types) nullOr path;
+  in { enable = mkEnableOption "Enable the hyprland window manager"; };
+
+  config = mkIf cfg.enable {
+    programs.hyprland = {
+      # Install the packages from nixpkgs
+      enable = true;
+      # Whether to enable XWayland
+      xwayland.enable = true;
+    };
+  };
+}
