4f6fdec50da67b78b42bad1485a5b0b4e8534493
Luca Leon Happel <lucahappel99@gmx.de>
2025-06-02 16:27:12 +0200

    add: garnix substitutor

--- modules/applications/nix-extras.nix
+++ modules/applications/nix-extras.nix
@@ -35,7 +35,7 @@
         substituters = [
           "https://nix-community.cachix.org/"
           "https://gvolpe-nixos.cachix.org"
-          #"https://cache.garnix.io"
+          "https://cache.garnix.io"
           "https://cuda-maintainers.cachix.org"
           "https://cache.nixos.org/"
           "https://lean4.cachix.org/"
@@ -43,7 +43,7 @@
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
-          #"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "lean4.cachix.org-1:mawtxSxcaiWE24xCXXgh3qnvlTkyU7evRRnGeAhD4Wk="
