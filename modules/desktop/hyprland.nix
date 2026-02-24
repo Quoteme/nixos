6a9b900b44d164d00f3f3a0667989b628e2695ab
Luca Leon Happel <lucahappel99@gmx.de>
2026-02-24 01:38:49 +0100

    add: gamescope and borders

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -12,14 +12,8 @@ in {
 
   config = with pkgs;
     mkIf cfg.enable {
-      nix.settings = {
-        substituters = [ "https://hyprland.cachix.org" ];
-        trusted-substituters = [ "https://hyprland.cachix.org" ];
-        trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        ];
-      };
       environment.systemPackages = [
+        libnotify
         blueman
         networkmanagerapplet
         cliphist
@@ -43,28 +37,23 @@ in {
         hyprpanel
         (pkgs.callPackage (import ../../pkgs/clipvault) { })
       ];
-      system.userActivationScripts.hyprland = {
-        text = ''
-          mkdir -p "$HOME/.config/hyprpanel/"
-          mkdir -p "$HOME/.config/hypr/"
-          mkdir -p "$HOME/.config/rofi/"
-          ln -sfn /etc/nixos/config/hyprland/hyprpanel/config.json "$HOME/.config/hyprpanel/config.json"
-          ln -sfn /etc/nixos/config/hyprland/hypridle/hypridle.conf "$HOME/.config/hypr/hypridle.conf"
-          ln -sfn /etc/nixos/config/rofi/config.rasi "$HOME/.config/rofi/config.rasi"
-          ln -sfn /etc/nixos/config/rofi/theme.rasi "$HOME/.config/rofi/theme.rasi"
-        '';
-        deps = [ ];
+      nix.settings = {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        ];
       };
-      programs.hyprlock.enable = true;
-      services.gnome.gnome-keyring.enable = true;
-      security.pam.services.login.enableGnomeKeyring = true;
-      security.pam.services.greetd.enableGnomeKeyring = true;
-      security.pam.services.greetd-password.enableGnomeKeyring = true;
-      services.logind.settings.Login = {
-        HandlePowerKey = "ignore";
-        HandlePowerKeyLongPress = "suspend-then-hibernate";
+
+      programs.dconf.enable = true;
+      programs.gamescope = {
+        enable = true;
+        env = {
+          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          __NV_PRIME_RENDER_OFFLOAD = "1";
+          __VK_LAYER_NV_optimus = "NVIDIA_only";
+        };
       };
-      services.hypridle.enable = true;
       programs.hyprland = {
         enable = true;
         plugins = plugins ++ [ pkgs.stable.hyprlandPlugins.hyprspace ];
@@ -82,16 +71,37 @@ in {
         xwayland.enable = true;
         withUWSM = true;
       };
+      programs.hyprlock.enable = true;
       programs.iio-hyprland.enable = true;
-      programs.dconf.enable = true;
+      security.pam.services.greetd-password.enableGnomeKeyring = true;
+      security.pam.services.greetd.enableGnomeKeyring = true;
+      security.pam.services.hyprlock = { };
+      security.pam.services.login.enableGnomeKeyring = true;
       services.dbus.packages = [ pkgs.dconf pkgs.gnome-keyring ];
+      services.gnome.gnome-keyring.enable = true;
+      services.hypridle.enable = true;
+      services.logind.settings.Login = {
+        HandlePowerKey = "ignore";
+        HandlePowerKeyLongPress = "suspend-then-hibernate";
+      };
 
-      services.xserver.updateDbusEnvironment = true;
+      services.upower.enable = true;
       services.xserver.displayManager.sessionCommands = ''
         eval $(gnome-keyring-daemon --start --daemonize --components=ssh,secrets)
         export SSH_AUTH_SOCK
       '';
-      services.upower.enable = true;
-      security.pam.services.hyprlock = { };
+      services.xserver.updateDbusEnvironment = true;
+      system.userActivationScripts.hyprland = {
+        text = ''
+          mkdir -p "$HOME/.config/hyprpanel/"
+          mkdir -p "$HOME/.config/hypr/"
+          mkdir -p "$HOME/.config/rofi/"
+          ln -sfn /etc/nixos/config/hyprland/hyprpanel/config.json "$HOME/.config/hyprpanel/config.json"
+          ln -sfn /etc/nixos/config/hyprland/hypridle/hypridle.conf "$HOME/.config/hypr/hypridle.conf"
+          ln -sfn /etc/nixos/config/rofi/config.rasi "$HOME/.config/rofi/config.rasi"
+          ln -sfn /etc/nixos/config/rofi/theme.rasi "$HOME/.config/rofi/theme.rasi"
+        '';
+        deps = [ ];
+      };
     };
 }
